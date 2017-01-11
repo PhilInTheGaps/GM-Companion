831d1a928c2823b7141908e8bd54da70c3f3339a
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-01-11 21:30:03 +0100

    Started putting functions into own classes.

---
+++ Player/src/application/HandleMetadata.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.scene.image.Image;
+
+public class HandleMetadata {
+	public static void handle(String key, Object value){
+		switch (key){
+    	case ("album"):
+    		Main.Album = value.toString();
+    		Main.albumLabel.setText("Album: " + Main.Album);
+			break;
+    	case ("artist"):
+    		Main.Artist = value.toString();
+    		Main.artistLabel.setText("Artist: " + Main.Artist);
+    		break;
+    	case ("title"):
+    		Main.Title = value.toString();
+    		Main.titleLabel.setText("Title: " + Main.Title);
+    		break;
+    	case ("year"):
+    		Main.Year = value.toString();
+    		Main.yearLabel.setText("Year: " + Main.Year);
+    		break;
+    	case ("image"):
+    		Image cover = (Image) value;
+    		Main.coverImage.setImage(cover);
+    		break;
+    	}
+	}
+}
