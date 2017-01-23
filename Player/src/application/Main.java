ab5215858a8e3b882603edd4a14e4ea1829a15f5
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-01-23 20:05:37 +0100

    Properly added jSoup to project

--- Player/src/application/Main.java
+++ Player/src/application/Main.java
@@ -1,17 +1,18 @@
 package application;
 
-import javafx.application.Application;
 import java.io.IOException;
+
+import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	
