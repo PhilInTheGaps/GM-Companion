e86ceb0cc2fde90a6ff4da832c75dc79e8c0719e
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-04-09 01:54:36 +0200

    GUI tests

--- PythonProject/src/root/nested/GM_Companion.py
+++ PythonProject/src/root/nested/GM_Companion.py
@@ -4,7 +4,65 @@ Created on 08.04.2017
 @author: Phil
 '''
 
-def main():
-    print("Test")
+from tkinter import *
+import ctypes
 
-main()
+class App:
+
+    def __init__(self, master):
+        pw = PanedWindow(orient="horizontal")
+        pw.pack(fill="both", expand=True)
+        
+        sidebar = PanedWindow(pw, orient="vertical")
+        pw.add(sidebar)
+        
+        main = Frame(pw, width=1280, height=720, background="grey")
+        pw.add(main)
+        
+        musicButtonsLabel = Label(sidebar, text="Music")
+        sidebar.add(musicButtonsLabel)
+        
+        musicButtons = Frame(sidebar, width=200, height=200, background="white")
+        sidebar.add(musicButtons)
+        
+        musicButtonPlay = Button(musicButtons, text="Play").pack(side=LEFT)
+        musicButtonPause = Button(musicButtons, text="Play").pack(side=LEFT)
+        musicButtonReplay = Button(musicButtons, text="Play").pack(side=LEFT)
+        musicButtonNext = Button(musicButtons, text="Play").pack(side=LEFT)
+        
+        soundButtonsLabel = Label(sidebar, text="Sound")
+        sidebar.add(soundButtonsLabel)
+        
+        soundButtons = Frame(sidebar, width=200, height=200, background="white")
+        sidebar.add(soundButtons)
+        
+        soundButtonPlay = Button(soundButtons, text="Play").pack(side=LEFT)
+        soundButtonPause = Button(soundButtons, text="Play").pack(side=LEFT)
+        soundButtonReplay = Button(soundButtons, text="Play").pack(side=LEFT)
+        soundButtonNext = Button(soundButtons, text="Play").pack(side=LEFT)
+        
+        sidebar.add(Label())
+        
+        # Creating Menu Bar
+        menubar = Menu(root)
+        menubar.add_command(label="GM-Help", command=self.say_hi)
+        menubar.add_command(label="Music", command=self.say_hi)
+        menubar.add_command(label="Sounds", command=self.say_hi)
+        menubar.add_command(label="Maps", command=self.say_hi)
+        menubar.add_command(label="Options", command=self.say_hi)
+        
+        # display the menu
+        root.config(menu=menubar)
+
+    def say_hi(self):
+        print("Hello there!")
+
+root = Tk()
+#root.geometry("1280x720+0+0")
+root.title("GM-Companion | Version: Beta 3.0 PRE-RELEASE (0.3.0.0)")
+root.iconbitmap(default="resources/icon256.ico")
+myappid = "GM-Companion (Python)"
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+app = App(root)
+
+root.mainloop()
