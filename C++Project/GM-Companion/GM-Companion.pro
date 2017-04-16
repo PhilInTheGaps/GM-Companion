b09f8cad10002b42c3e3c0438d8d9aba004f2f15
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-04-17 01:26:35 +0200

    Started to work on c++ project, only testing so far

--- C++Project/GM-Companion/GM-Companion.pro
+++ C++Project/GM-Companion/GM-Companion.pro
@@ -5,6 +5,7 @@
 #-------------------------------------------------
 
 QT       += core gui
+QT       += multimedia
 
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
@@ -24,7 +25,8 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    musicbuttons.cpp
 
 HEADERS  += mainwindow.h
 
--- C++Project/GM-Companion/mainwindow.cpp
+++ C++Project/GM-Companion/mainwindow.cpp
@@ -1,14 +1,38 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QVector>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+//    getTextFile();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+//void MainWindow::on_goButton_clicked()
+//{
+//    QString word = ui->lineEdit->text();
+//    ui->textEdit->find(word, QTextDocument::FindWholeWords);
+//}
+
+//void MainWindow::getTextFile(){
+//    QFile myFile(":/textFiles/testTextFile.txt");
+//    myFile.open(QIODevice::ReadOnly);
+
+//    QTextStream textStream(&myFile);
+//    QString line = textStream.readAll();
+//    myFile.close();
+
+//    ui->textEdit->setPlainText(line);
+//    QTextCursor textCursor = ui->textEdit->textCursor();
+//    textCursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+//}
