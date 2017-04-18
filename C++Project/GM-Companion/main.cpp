ce94181de9097e8ae2a81f336963e6a9c6f2331e
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-04-18 02:58:23 +0200

    Added Music Button Generation and Music Playback

--- C++Project/GM-Companion/main.cpp
+++ C++Project/GM-Companion/main.cpp
@@ -1,74 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
-//#include <QWidget>
-//#include <QString>
-//#include <QVector>
-//#include <QHBoxLayout>
-//#include <QVBoxLayout>
-//#include <QGridLayout>
-//#include <QLabel>
-//#include <QPushButton>
-//#include <QDir>
-//#include <QMediaPlayer>
-
-
-
-
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
-
-
     w.show();
-
-    //Builds Window
-//    QWidget *mainWindow = new QWidget;
-//    mainWindow->setWindowTitle("GM-Companion | C++ Alpha Version");
-//    mainWindow->setMinimumWidth(1280);
-//    mainWindow->setMinimumHeight(720);
-//    mainWindow->show();
-
-//    QHBoxLayout *mainHBox = new QHBoxLayout;
-//    mainWindow->setLayout(mainHBox);
-
-//    //Creates Info Box with content
-//    QVBoxLayout *infoBox = new QVBoxLayout;
-//    mainHBox->addLayout(infoBox);
-
-//    QLabel *musicLabel = new QLabel("Music");
-//    infoBox->addWidget(musicLabel);
-
-//    QHBoxLayout *musicControls = new QHBoxLayout;
-//    QPushButton *musicPlayButton = new QPushButton("Play");
-//    musicControls->addWidget(musicPlayButton);
-//    QPushButton *musicReplayButton = new QPushButton("Replay");
-//    musicControls->addWidget(musicReplayButton);
-//    QPushButton *musicNextButton = new QPushButton("Next");
-//    musicControls->addWidget(musicNextButton);
-//    infoBox->addLayout(musicControls);
-
-//    QLabel *soundLabel = new QLabel("Sound");
-//    infoBox->addWidget(soundLabel);
-
-//    //Grid that contains all the music Buttons
-//    QGridLayout *buttonGrid = new QGridLayout;
-//    mainHBox->addLayout(buttonGrid);
-
-
-
-//    QStringList musicFolders = getFolders("C:/Users/Phil/Google Drive/GM Boys/GM-Companion/Music/Fantasy");
-//    for (QString s : musicFolders)
-//    {
-//        if (!s.contains("."))
-//        {
-//            QPushButton *b = new QPushButton(s);
-//            //QObject::connect(&b, SIGNAL (clicked()), &app, SLOT (playMusic()));
-//        }
-//    }
-
-//    playMusic();
-
     return app.exec();
 }
--- C++Project/GM-Companion/mainwindow.h
+++ C++Project/GM-Companion/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QMediaPlayer>
+#include <QMediaPlayerControl>
+#include <QMediaPlaylist>
+#include <QUrl>
+#include <QSignalMapper>
 
 namespace Ui {
 class MainWindow;
@@ -14,12 +20,36 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void addToPlaylist(QUrl url);
 
 private slots:
-    void playMusic();
+    void playMusic(QString);
+
+    void on_musicPauseButton_clicked();
+
+    void on_musicPlayButton_clicked();
+
+    void on_musicReplayButton_clicked();
+
+    void on_musicNextButton_clicked();
+
+    void on_setResourcesFolder_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    QString musicPath;
+    QString soundPath;
+    QString mapsPath;
+    QString resourcesPath;
+
+    QSignalMapper *signalMapper;
+
+    QMediaPlayer *musicPlayer;
+    QMediaPlaylist *musicPlaylist;
+
+    QMediaPlayer *soundPlayer;
+    QMediaPlaylist *soundPlaylist;
 };
 
 #endif // MAINWINDOW_H
