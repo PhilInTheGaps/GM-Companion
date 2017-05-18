44e0f47a1ce0a749b063bf197c690d91eafc4a65
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-05-19 00:51:14 +0200

    Included blog feed and made general improvements

--- C++Project/GM-Companion/dicemanager.h
+++ C++Project/GM-Companion/dicemanager.h
@@ -22,16 +22,31 @@ public:
 
     void setSidesList(QList<int>);
 
+    void addDice(int);
+
+    void resetDice();
+
 private slots:
     void rollDice(int);
 
+    void on_addButton_clicked();
+
+    void on_resetButton_clicked();
+
 private:
     QList<int> sidesList = {2, 3, 4, 6, 12, 20};
+
+    void generateDice();
+    void clearDice();
 
     QSignalMapper *signalMapperDice;
 
     QFrame* diceFrame;
     QHBoxLayout* diceFrameLayout;
+
+    QFrame* customButtonFrame;
+    QHBoxLayout* customButtonLayout;
+    QSpinBox* customButtonSpinBox;
 
     QFrame* leftFrame;
     QVBoxLayout* leftLayout;
