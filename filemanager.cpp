2af2f85b213ad283aecca614d13c0e0ad5e6c7cb
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-07-07 21:49:12 +0200

    Improved file copying / deleting
    Former-commit-id: e62c7920df08319c0d278e26385d7350008dd0da
    Former-commit-id: 253b23eec9727eb7143caeb7696ff10b6abc8cbe

--- filemanager.cpp
+++ filemanager.cpp
@@ -3,20 +3,81 @@
 #include <QDebug>
 #include <QDir>
 #include <functions.h>
+#include <QFileInfo>
+#include <QDateTime>
 
 FileManager::FileManager(){
 
 }
 
+// Removes Directories and Files
+bool removeDir(const QString & dirName)
+{
+    bool result = true;
+    QDir dir(dirName);
+
+    if (dir.exists()) {
+        Q_FOREACH(QFileInfo info, dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst)) {
+            if (info.isDir()) {
+                result = removeDir(info.absoluteFilePath());
+            }
+            else {
+                result = QFile::remove(info.absoluteFilePath());
+            }
+
+            if (!result) {
+                return result;
+            }
+        }
+        result = QDir().rmdir(dirName);
+    }
+    return result;
+}
+
+// Copies a file if it is newer than the existing one
+void FileManager::copyContents(QString path, QString destination){
+
+    bool copy = true;
+
+    if (QFile(destination).exists()){
+        QFileInfo oldInfo (path);
+        QDateTime oldTime (oldInfo.lastModified());
+
+        QFileInfo newInfo (destination);
+        QDateTime newTime (newInfo.lastModified());
+
+        if (newTime.operator <=(oldTime)){
+            copy = false;
+            qDebug() << path + " is newer, replacing...";
+        }
+    } else {
+        qDebug() << "copying " + path;
+    }
+
+    if (copy)
+        QFile::copy(path, destination);
+}
+
 void FileManager::copyFiles(){
+    // Local home directory
     QDir lDir(QDir::homePath()+"/.gm-companion");
+
+    // List of all directories to be created
+    QStringList dirList = {"addons", "characters", "maps", "music", "notes", "resources", "sounds", "styles", "notes"};
+
+    // List of all files to be deleted
+//    filesToBeDeleted = {""};
+
+    // List of all folders to be deleted
+    foldersToBeDeleted.append({"names/DSA5", "names/SIFRP"});
+
+    // Check if local .gm-companion directory exists and create it if not
     if (!lDir.exists()){
         qDebug() << QDir::homePath()+"/.gm-companion"+QCoreApplication::translate("FileManager","does not exist. Creating...");
         lDir.mkpath(".");
     }
 
-    QStringList dirList = {"music", "sounds", "maps", "characters", "names", "addons", "resources", "styles", "notes"};
-
+    // Check if subdirs exist and create them if not
     for (QString path : dirList){
         QDir dir(QDir::homePath()+"/.gm-companion/"+path);
         if (!dir.exists()){
@@ -28,62 +89,37 @@ void FileManager::copyFiles(){
     bool copied = false;
     qDebug() << QCoreApplication::translate("FileManager" ,"Copying files to ") << QDir::homePath()+"/.gm-companion ...";
 
-    // Checks the OS and copies the files to a writable directory accordingly
+    // Actually copy files to the directories
     #ifdef __linux__
-    for (QString folder : getFolders("/usr/share/gm-companion/names")){
-        if (!folder.contains(".")){
-            QDir dir(QDir::homePath()+"/.gm-companion/names/"+folder);
-            if (!dir.exists()){
-                dir.mkpath(".");
-            }
-
-            for (QString subfolder : getFolders("/usr/share/gm-companion/names/"+folder)){
-                if (!subfolder.contains(".")){
-                    QDir dir2(QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder);
-                    if(!dir2.exists()){
-                        dir2.mkpath(".");
-                    }
-
-                    for (QString file : getFiles("/usr/share/gm-companion/names/"+folder+"/"+subfolder)){
-                        QFile f(QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder+"/"+file);
-                        if (!f.exists()){
-                            copied = true;
-                            QFile::copy("/usr/share/gm-companion/names/"+folder+"/"+subfolder+"/"+file, QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder+"/"+file);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    for (QString style : getFiles("/usr/share/gm-companion/styles")){
-        QFile f(QDir::homePath()+"/.gm-companion/styles/"+style);
-        if (!f.exists()){
-            copied = true;
-            QFile::copy("/usr/share/gm-companion/styles/"+style, QDir::homePath()+"/.gm-companion/styles/"+style);
-        }
-    }
-
-    QFile f(QDir::homePath()+"/.gm-companion/units.ini");
-    if (!f.exists()){
-        copied = true;
-        QFile::copy("/usr/share/gm-companion/units.ini", QDir::homePath()+"/.gm-companion/units.ini");
-    }
-
-    QFile f2(QDir::homePath()+"/.gm-companion/radios.ini");
-    if (!f2.exists()){
-        copied = true;
-        QFile::copy("/usr/share/gm-companion/radios.ini", QDir::homePath()+"/.gm-companion/radios.ini");
-    }
+    origPath = "/usr/share/gm-companion";
     #elif _WIN32
-    for (QString folder : getFolders(QApplication::applicationDirPath()+"/names")){
+    origPath = QApplication::applicationDirPath();
+    #else
+    origPath = QApplication::applicationDirPath();
+    qDebug() << QCoreApplication::translate("FileManager","This OS is not supported.");
+    #endif
+
+    // Copy folder contents
+    for (QString folder : dirList){
+        QString topPathString = origPath + "/" + folder;
+        QDir topPath(topPathString);
+
+        if (topPath.exists()){
+            for (QString file : getFiles(topPathString)){
+                copyContents(topPathString+"/"+file, origPath+"/"+folder+"/"+file);
+            }
+        }
+    }
+
+    // Copy names
+    for (QString folder : getFolders(+"/names")){
         if (!folder.contains(".")){
-            QDir dir(QDir::homePath()+"/.gm-companion/names/"+folder);
+            QDir dir(origPath+"/names/"+folder);
             if (!dir.exists()){
                 dir.mkpath(".");
             }
 
-            for (QString subfolder : getFolders(QApplication::applicationDirPath()+"/names/"+folder)){
+            for (QString subfolder : getFolders(origPath+"/names/"+folder)){
                 if (!subfolder.contains(".")){
                     QDir dir2(QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder);
                     if(!dir2.exists()){
@@ -94,7 +130,7 @@ void FileManager::copyFiles(){
                         QFile f(QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder+"/"+file);
                         if (!f.exists()){
                             copied = true;
-                            QFile::copy(QApplication::applicationDirPath()+"/names/"+folder+"/"+subfolder+"/"+file, QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder+"/"+file);
+                            QFile::copy(origPath+"/names/"+folder+"/"+subfolder+"/"+file, QDir::homePath()+"/.gm-companion/names/"+folder+"/"+subfolder+"/"+file);
                         }
                     }
                 }
@@ -102,35 +138,28 @@ void FileManager::copyFiles(){
         }
     }
 
-    for (QString style : getFiles(QApplication::applicationDirPath()+"/styles")){
-        QFile f(QDir::homePath()+"/.gm-companion/styles/"+style);
-        if (!f.exists()){
-            copied = true;
-            QFile::copy(QApplication::applicationDirPath()+"/styles/"+style, QDir::homePath()+"/.gm-companion/styles/"+style);
+    // Delete obsolete Files
+    if (!filesToBeDeleted.isEmpty()){
+        for (QString file : filesToBeDeleted){
+            QFile f (QDir::homePath()+"/.gm-companion/"+file);
+
+            if (f.exists()){
+                f.remove();
+            }
         }
     }
 
-    QFile f(QDir::homePath()+"/.gm-companion/units.ini");
-    if (!f.exists()){
-        copied = true;
-        QFile::copy(QApplication::applicationDirPath()+"/units.ini", QDir::homePath()+"/.gm-companion/units.ini");
+    // Delete obsolete Folders
+    if (!foldersToBeDeleted.isEmpty()){
+        for (QString folder : foldersToBeDeleted){
+            if (QDir(QDir::homePath()+"/.gm-companion/"+folder).exists()){
+                qDebug() << "Removing " + QDir::homePath()+"/.gm-companion/"+folder;
+
+                removeDir(QDir::homePath()+"/.gm-companion/"+folder);
+            }
+        }
     }
 
-    QFile f2(QDir::homePath()+"/.gm-companion/radios.ini");
-    if (!f2.exists()){
-        copied = true;
-        QFile::copy(QApplication::applicationDirPath()+"/radios.ini", QDir::homePath()+"/.gm-companion/radios.ini");
-    }
-
-
-    #else
-        qDebug() << QCoreApplication::translate("FileManager","This OS is not supported.");
-    #endif
-
-    if (copied){
-        qDebug() << QCoreApplication::translate("FileManager","Done.");
-    }else{
-        qDebug() << QCoreApplication::translate("FileManager","Files already exist.");
-    }
+    qDebug() << QCoreApplication::translate("FileManager","Done.");
 
 }
