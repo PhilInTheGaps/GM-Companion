ef4d33e8226fd91b99458d0e46ff587d0fd55f35
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-08-04 00:24:46 +0200

    Added DiceTool as the DiceManager successor
    Former-commit-id: 73174388b0db91df2ab4a796187163266fd91c74
    Former-commit-id: 895b8f4c4aff69254dab1b214924d0ac62736480

--- tools/dicetool.h
+++ tools/dicetool.h
@@ -2,6 +2,8 @@
 #define DICETOOL_H
 
 #include <QWidget>
+#include <QList>
+#include <QSignalMapper>
 
 namespace Ui {
 class DiceTool;
@@ -15,8 +17,21 @@ public:
     explicit DiceTool(QWidget *parent = 0);
     ~DiceTool();
 
+private slots:
+    void rollDice(int sides);
+
+    void on_pushButton_addDie_clicked();
+
+    void on_pushButton_resetDice_clicked();
+
 private:
     Ui::DiceTool *ui;
+
+    QSignalMapper *signalMapperDice;
+
+    QList<int> sidesList = {2, 3, 4, 6, 12, 20};    // Default Dice
+
+    void generateDice();
 };
 
 #endif // DICETOOL_H
