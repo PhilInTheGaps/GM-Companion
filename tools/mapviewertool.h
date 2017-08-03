b1531c113e0c129dca32e2907c26eac0174cefe0
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-08-04 01:56:09 +0200

    Added MapViewerTool as the "Maps" successor

---
+++ tools/mapviewertool.h
@@ -0,0 +1,43 @@
+#ifndef MAPVIEWERTOOL_H
+#define MAPVIEWERTOOL_H
+
+#include <QWidget>
+#include <QSignalMapper>
+#include <QLabel>
+
+namespace Ui {
+class MapViewerTool;
+}
+
+class MapViewerTool : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MapViewerTool(QWidget *parent = 0);
+    ~MapViewerTool();
+
+private slots:
+    void on_pushButton_zoomIn_clicked();
+
+    void on_pushButton_zoomOut_clicked();
+
+    void on_pushButton_fitToView_clicked();
+
+    void on_pushButton_resetSize_clicked();
+
+    void setMap(QString mapPath);
+
+private:
+    Ui::MapViewerTool *ui;
+
+    QLabel *mapLabel;
+
+    QSignalMapper *signalMapperMaps;
+
+    double mapsZoomFactor = 1.0;
+
+    void getMaps();
+};
+
+#endif // MAPVIEWERTOOL_H
