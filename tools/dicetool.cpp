468c9c6663dbd4a926122466993c075935867799
PhilInTheGaps <philhoffmann1337@gmail.com>
2017-08-04 00:58:50 +0200

    Added bonus dice function
    Former-commit-id: c85a96a8d85af2e2e250c2b6da1f40fa3e974a01
    Former-commit-id: ad68d49f45e01f7f2ebf875ba0bed04a3ae8657c

--- tools/dicetool.cpp
+++ tools/dicetool.cpp
@@ -28,6 +28,8 @@ void DiceTool::rollDice(int sides){
 
     ui->tableWidget_results->insertRow(0);
 
+    QList<int> results;
+
     for (int i = 0; i<amount; i++){
         QTableWidgetItem *i1 = new QTableWidgetItem;   // dice
         QTableWidgetItem *i2 = new QTableWidgetItem;   // roll
@@ -53,6 +55,40 @@ void DiceTool::rollDice(int sides){
         ui->tableWidget_results->setItem(0, 0, i1);
         ui->tableWidget_results->setItem(0, 1, i2);
         ui->tableWidget_results->setItem(0, 2, i3);
+
+        // Add temp result to results list
+        results.push_back(temp);
+    }
+
+    // Remove bonus dice
+    for (int i = 0; i<ui->spinBox_bonusDice->value(); i++)
+    {
+        QTableWidgetItem *i1 = new QTableWidgetItem;   // dice
+        QTableWidgetItem *i2 = new QTableWidgetItem;   // roll
+        QTableWidgetItem *i3 = new QTableWidgetItem;   // result
+
+        // Get smallest result
+        int min = *std::min_element(results.begin(), results.end());
+        result -= min;
+
+        // Set dice table item
+        i1->setText("Bonus #"+QString::number(i+1));
+
+        // Set the roll table item
+        i2->setText("-" + QString::number(min));
+
+        // Display result
+        i3->setText(QString::number(result));
+
+        // Insert a new table row
+        ui->tableWidget_results->insertRow(0);
+
+        // Add items to first row
+        ui->tableWidget_results->setItem(0, 0, i1);
+        ui->tableWidget_results->setItem(0, 1, i2);
+        ui->tableWidget_results->setItem(0, 2, i3);
+
+        results.removeOne(min);
     }
 
     // Add modifier
