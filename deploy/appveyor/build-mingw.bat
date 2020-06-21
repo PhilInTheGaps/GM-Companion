a7ff7b0ddb0a046659c51d166a851b32ceb532e9
Phil Hoffmann <phil.hoffmann@zoho.eu>
2020-06-21 16:33:44 +0200

    Install pkgconfig via cget in appveyor build

--- deploy/appveyor/build-mingw.bat
+++ deploy/appveyor/build-mingw.bat
@@ -4,6 +4,7 @@ mkdir build
 cd build
 
 cget init --shared -DCMAKE_SH="CMAKE_SH-NOTFOUND"
+cget install pfultz2/pkgconfig
 cget install -G "MinGW Makefiles" --release -DCMAKE_C_FLAGS="-fno-asynchronous-unwind-tables" -f ../requirements.txt || EXIT /B 1
 
 cmake .. -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=install || EXIT /B 1
