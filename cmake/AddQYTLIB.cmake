87093ad161b411b987af814a208dde8a858322dd
Phil Hoffmann <philhoffmann1337@gmail.com>
2020-10-21 17:12:30 +0200

    Set cmake to build qytlib as a static library

--- cmake/AddQYTLIB.cmake
+++ cmake/AddQYTLIB.cmake
@@ -5,4 +5,6 @@ if(NOT EXISTS ${EXTERNAL_LIB_DIR}/qytlib/README.md)
 endif()
 
 set(QYTLIB_BUILD_TESTS OFF CACHE BOOL "Disable qytlib tests")
+set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build static libs")
+
 add_subdirectory(${EXTERNAL_LIB_DIR}/qytlib ${EXTERNAL_LIB_DIR}/qytlib)
