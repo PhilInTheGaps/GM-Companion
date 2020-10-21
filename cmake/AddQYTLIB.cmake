560a7fd02e5376ce41eaa8b8ed80552ca4329ddb
Phil Hoffmann <philhoffmann1337@gmail.com>
2020-10-21 16:45:18 +0200

    Updated qytlib, disabled qytlib tests

--- cmake/AddQYTLIB.cmake
+++ cmake/AddQYTLIB.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required(VERSION 3.1.0)
 
 if(NOT EXISTS ${EXTERNAL_LIB_DIR}/qytlib/README.md)
-    message(FATAL_ERROR "The QYTLIB submodule was not downloaded! Please run  git submodules update --init --recursive  and try again.")
+    message(FATAL_ERROR "The QYTLIB submodule was not downloaded! Please run `git submodules update --init --recursive` and try again.")
 endif()
 
+set(QYTLIB_BUILD_TESTS OFF CACHE BOOL "Disable qytlib tests")
 add_subdirectory(${EXTERNAL_LIB_DIR}/qytlib ${EXTERNAL_LIB_DIR}/qytlib)
