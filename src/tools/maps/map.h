a60a2b778fbc1e72e6b7b1d02fbbf6626bdd7be0
Phil Hoffmann <philhoffmann1337@gmail.com>
2021-08-26 22:48:46 +0200

    Map tool now uses the new file API

--- src/tools/maps/map.h
+++ src/tools/maps/map.h
@@ -6,30 +6,23 @@
 #include <QAbstractListModel>
 
 #include "mapmarker.h"
+#include "thirdparty/propertyhelper/PropertyHelper.h"
 
 class Map : public QObject
 {
     Q_OBJECT
+    AUTO_PROPERTY(QString, name)
+    AUTO_PROPERTY(QString, path)
+    AUTO_PROPERTY(QString, imageData)
 
 public:
-    explicit Map(QString name, QString path, QObject *parent = nullptr);
-    ~Map();
-
-    Q_PROPERTY(QString name READ name NOTIFY nameChanged)
-    QString name() const { return m_name; }
-    void setName(QString name) { m_name = name; emit nameChanged(); }
-
-    Q_PROPERTY(QString path READ path NOTIFY pathChanged)
-    QString path() const { return m_path; }
-    void setPath(QString path) { m_path = path; emit pathChanged(); }
-
-    Q_PROPERTY(QString imageData READ data NOTIFY dataChanged)
-    QString data() const { return m_data; }
-    void setData(QString data) { m_data = data; emit dataChanged(); }
+    explicit Map(const QString &name, const QString &path, QObject *parent);
 
     Q_PROPERTY(MapMarkerModel* markers READ markers NOTIFY markersChanged)
+
     Q_PROPERTY(bool hasMarkers READ hasMarkers NOTIFY markersChanged)
     bool hasMarkers() const { return !m_markers->isEmpty(); }
+
     MapMarkerModel* markers() const { return m_markers; }
     void setMarkers(MapMarkerModel* markers) { m_markers = markers;  }
     void addMarker(MapMarker* marker) { m_markers->addElement(marker); }
@@ -39,21 +32,20 @@ public:
     void deleteMarker(int index);
 
 signals:
-    void nameChanged();
-    void pathChanged();
     void markersChanged();
-    void dataChanged();
 
 private:
-    QString m_name, m_path, m_data;
     MapMarkerModel* m_markers;
     bool m_wasLoaded = false;
 };
 
 // Model for QML
-class MapListModel : public QAbstractListModel {
+class MapListModel : public QAbstractListModel
+{
     Q_OBJECT
 public:
+    explicit MapListModel(QObject *parent) : QAbstractListModel(parent) {}
+
     int rowCount(const QModelIndex&) const override { return m_items.size(); }
     QVariant data(const QModelIndex& index, int role) const override;
     int size() const { return m_items.size(); }
@@ -77,14 +69,12 @@ private:
 class MapCategory : public QObject
 {
     Q_OBJECT
+    AUTO_PROPERTY(QString, name)
+    AUTO_PROPERTY(QList<Map*>, maps)
 public:
-    MapCategory(QString name, QList<Map*> maps, QObject *parent = nullptr);
+    MapCategory(const QString &name, const QList<Map *> &maps, QObject *parent);
     virtual ~MapCategory();
 
-    QString name() const { return m_name; }
-
-    QList<Map *> maps() const { return m_maps; }
-    void setMaps(QList<Map*> maps) { m_maps = maps; }
     void addMap(Map* map);
     void loadMaps();
 
@@ -92,8 +82,6 @@ signals:
     void loadedMaps(QString category);
 
 private:
-    QString m_name;
-    QList<Map*> m_maps;
     int m_getFileListRequestId = -1;
     bool m_wasLoaded = false;
 };
