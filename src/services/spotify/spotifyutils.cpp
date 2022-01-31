90ef849bc2d096e2a0f443201bf35d2296b9d241
Phil Hoffmann <philhoffmann1337@gmail.com>
2022-01-31 16:48:58 +0100

    Improved spotify utils to also work with open.spotify.com urls instead of only internal uris

--- src/services/spotify/spotifyutils.cpp
+++ src/services/spotify/spotifyutils.cpp
@@ -1,5 +1,6 @@
 #include "spotifyutils.h"
 #include <QStringList>
+#include <QRegularExpression>
 
 /**
  * @brief Get the type of a spotify uri.
@@ -7,31 +8,40 @@
  * @return 0: Album, 1: Playlist, 2: Track, 3: Artist, 4: episode, 5: show -1:
  * unknown
  */
-auto SpotifyUtils::getUriType(const QString& uri)->int
+auto SpotifyUtils::getUriType(const QString& uri) -> SpotifyType
 {
-    if (uri.contains("album:")) return 0;
+    if (uri.contains("album:") || uri.contains("album/")) return Album;
 
-    if (uri.contains("playlist:")) return 1;
+    if (uri.contains("playlist:") || uri.contains("playlist/")) return Playlist;
 
-    if (uri.contains("track:")) return 2;
+    if (uri.contains("track:") || uri.contains("track/")) return Track;
 
-    if (uri.contains("artist:")) return 3;
+    if (uri.contains("artist:") || uri.contains("artist/")) return Artist;
 
-    if (uri.contains("episode:")) return 4;
+    if (uri.contains("episode:") || uri.contains("episode/")) return Episode;
 
-    if (uri.contains("show:")) return 5;
+    if (uri.contains("show:") || uri.contains("show/")) return Show;
 
-    return -1;
+    return Unknown;
 }
 
 /**
  * @brief Get Spotify ID from Spotify URI
  * @param uri Spotify URI like spotify:track:6rqhFgbbKwnb9MLmUQDhG6
+ * or https://open.spotify.com/track/0W35nxtHtFlseSojmygEsf?si=43b918d258c540ad
  * @return Spotify ID like 6rqhFgbbKwnb9MLmUQDhG6
  */
-auto SpotifyUtils::getIdFromUri(QString uri)->QString
+auto SpotifyUtils::getIdFromUri(const QString &uri)->QString
 {
-    return uri = uri.right(uri.count() - uri.lastIndexOf(":") - 1);
+    const auto regex = QRegularExpression(ID_FROM_URI);
+    const auto match = regex.match(uri);
+
+    if (match.isValid() && match.hasMatch())
+    {
+        return match.captured(2);
+    }
+
+    return {};
 }
 
 /**
@@ -41,21 +51,13 @@ auto SpotifyUtils::getIdFromUri(QString uri)->QString
  */
 auto SpotifyUtils::getIdFromHref(const QString& href)->QString
 {
-    auto split = href.split("/");
-    auto index = -1;
+    const auto regex = QRegularExpression(ID_FROM_HREF);
+    const auto match = regex.match(href);
 
-    if (split.contains("albums"))
+    if (match.isValid() && match.hasMatch())
     {
-        index = split.indexOf("albums") + 1;
-    }
-    else if (split.contains("playlists"))
-    {
-        index = split.indexOf("playlists") + 1;
-    }
-    else if (split.contains("tracks"))
-    {
-        index = split.indexOf("tracks") + 1;
+        return match.captured(2);
     }
 
-    return split[index];
+    return {};
 }
--- src/services/spotify/spotifyutils.h
+++ src/services/spotify/spotifyutils.h
@@ -1,11 +1,28 @@
 #pragma once
 
 #include <QString>
+#include <QObject>
 
 class SpotifyUtils
 {
 public:
-    static int getUriType(const QString& uri);
-    static QString getIdFromUri(QString uri);
-    static QString getIdFromHref(const QString& href);
+    enum SpotifyType {
+        Album,
+        Playlist,
+        Track,
+        Artist,
+        Episode,
+        Show,
+        Unknown = -1
+    };
+
+    static auto getUriType(const QString& uri) -> SpotifyType;
+    static auto getIdFromUri(const QString &uri) -> QString;
+    static auto getIdFromHref(const QString& href) -> QString;
+
+private:
+    static constexpr const char* ID_FROM_URI = R"((\/(?!.*\/)(?<=\/)|:(?!.*\/)(?!.*:)(?<=:))([[:alnum:]]+))";
+    static constexpr const char* ID_FROM_HREF =R"((albums|playlists|tracks|artists|episodes|shows)\/([[:alnum:]]+))";
 };
+
+Q_DECLARE_METATYPE(SpotifyUtils::SpotifyType)
