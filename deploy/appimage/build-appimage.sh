14c9a2b28d86ee205e27bf7d3005df51029f6b9a
Phil Hoffmann <philhoffmann1337@gmail.com>
2023-08-18 16:20:59 +0200

    Added steps to CI to build an appimage again

--- deploy/appimage/build-appimage.sh
+++ deploy/appimage/build-appimage.sh
@@ -2,14 +2,6 @@
 
 echo "Building AppImage ..."
 
-# build librespot
-echo "Building librespot ..."
-git clone https://github.com/librespot-org/librespot
-cd librespot
-cargo build --release
-cp target/release/librespot ../AppDir/usr/bin/librespot
-cd ..
-
 # Remove include dir
 echo "Removing include dir ..."
 rm -rf AppDir/usr/include
@@ -32,23 +24,13 @@ if [ ! -f linuxdeploy-plugin-qt-x86_64.AppImage ]; then
     chmod u+x linuxdeploy-plugin-qt-x86_64.AppImage
 fi
 
-# Download linuxdeploy gstreamer plugin
-echo "Downloading linuxdeploy gstreamer plugin ..."
-if [ ! -f linuxdeploy-plugin-gstreamer.sh ]; then
-    wget https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gstreamer/master/linuxdeploy-plugin-gstreamer.sh
-    chmod u+x linuxdeploy-plugin-gstreamer.sh
-fi
-
-export EXTRA_QT_PLUGINS=audio,imageformats,mediaservice,playlistformats,gui
-export QML_MODULES_PATHS=/opt/qt512/qml/
 export QML_SOURCES_PATHS=../app/ui/
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH;cget/lib;cget/lib/x86_64-linux-gnu"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:cget/lib:cget/lib/x86_64-linux-gnu:thirdparty/sentry-native"
 
 # Build AppImage
 ./linuxdeploy-x86_64.AppImage \
   --appdir AppDir \
   -e AppDir/usr/bin/librespot \
   -d AppDir/usr/share/applications/lol.rophil.gm_companion.desktop \
-  --plugin gstreamer \
   --plugin qt \
   --output appimage
